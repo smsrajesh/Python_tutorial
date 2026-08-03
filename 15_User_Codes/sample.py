85ea296fd8867191864add725c350d9c174fa7d1
smsrajesh <smsrajeshrangisetti369@gmail.com>
2026-08-03 20:04:07 +0530

    modified some folders and files

---
+++ 15_User_Codes/sample.py
@@ -0,0 +1,37 @@
+# ---------------------------------------------------
+# Problem: Longest Consecutive Sequence
+# Category: Arrays & Hashing
+# Difficulty: Medium
+# Approach: Sorting (Brute Force)
+# Time Complexity: O(n log n)
+# Space Complexity: O(1) (using sort()) / O(n) if using sorted()
+# ---------------------------------------------------
+
+# Example 1 : 
+nums = [5, 4, 6, 1, 3, 2] 
+# Output = 4
+
+
+def Longest_Consecutive_Sequence(nums):
+
+    if nums:
+        nums.sort()
+        current=1
+        longest=1
+        for i in range(1,len(nums)):
+
+            diff = nums[i]-nums[i-1]
+            if diff==1:
+                current+=1
+            elif diff==0:
+                continue
+            elif diff>1:
+                current=1
+
+            longest=max(longest,current)
+    else:
+        return 0
+    
+    return longest
+
+print(Longest_Consecutive_Sequence(nums))
